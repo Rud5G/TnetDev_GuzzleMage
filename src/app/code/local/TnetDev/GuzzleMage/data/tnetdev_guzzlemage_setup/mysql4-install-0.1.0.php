d0d410870206fc84b28c58e3ceadfb0139cce222
Rudger Gravestein <rudgergravestein@gmail.com>
2014-01-10 21:34:56 +0100

    Add migrationscript

---
+++ src/app/code/local/TnetDev/GuzzleMage/data/tnetdev_guzzlemage_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+//uncomment + remove the entry from core_resource table.
+
+//$installer->setConfigData('crentails/github/username', '');
+//$installer->setConfigData('crentails/github/password', '');
+
+$installer->endSetup();
